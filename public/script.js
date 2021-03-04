5b09c0f18b805f492f2f6e3f44d4706b1bcca989
Draugsvoll <ove.hd91@gmail.com>
2021-03-04 14:49:05 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,5 +2,5 @@ function openLink () {
     // var win = window.location.replace("https://ove-portfolio.herokuapp.com/")
     // win.focus()
 
-    window.open("https://ove-portfolio.herokuapp.com/",'_blank');
+    window.open("https://ove-portfolio.netlify.app/",'_blank');
 }
